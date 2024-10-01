a2123ee2671ea96148303cf83c96f2c12581a68a
Tony Chan <tony.chan@vonage.com>
2024-10-01 12:24:40 -0700

    initial

---
+++ README.md
@@ -0,0 +1,58 @@
+# WebSocket server (aka Middleware server)
+
+You may use this sample code as a middleware server to handle WebSockets connections and receive audio over those WebSokets.
+
+Of course, in real reployment, your middleware application is an intermediary platform to connect to ASR engines, TTS engines, Voice bots, Voice AI, and more, and may also send back audio to the Vonage platform.
+
+In this sample code, we use Deepgram as an ASR engine.
+
+## About this sample server code
+
+This middleware server makes use of the [WebSockets feature](https://developer.vonage.com/en/voice/voice-api/concepts/websockets) of Vonage Voice API. When a voice call is established, a Voice API application triggers a WebSocket connection to this server and streams the audio from the voice call(s) in real time.
+
+See https://github.com/nexmo-se/pstn-websocket-app-variant-3 for a **sample Voice API application** using this basic middleware server code to connect voice calls.
+
+## Set up
+
+### Set up this server code - Host server public hostname and port
+
+First set up this server from https://github.com/nexmo-se/websocket-server-variant-3.
+
+Default local (not public!) reference connection code `port` is: 6000.
+
+If you plan to test using `Local deployment` with ngrok (Internet tunneling service) for both this server code and the sample Voice API application, you may set up [multiple ngrok tunnels](https://ngrok.com/docs/agent/config/#tunnel-configurations).
+
+For the next steps, you will need:
+- This server's public hostname and if necessary public port,</br>
+e.g. `xxxxxxxx.ngrok.io`, `xxxxxxxx.herokuapp.com`, `myserver.mycompany.com:32000` (will be needed when setting up the sample Voice API application side) ,</br>
+no `port` is necessary with ngrok or heroku as public hostname.</br>
+
+Have Node.js installed on your system, this application has been tested with Node.js version 18.19.1<br>
+
+Install node modules with the command:<br>
+ ```bash
+npm install
+```
+
+To illustrate using Deepgram as an ASR (Automatic Speech Recognition) engine with this middleware, you may use a Deepgram API key (see https://developers.deepgram.com/docs/create-additional-api-keys)
+
+Copy or rename .env-example to .env, edit that file and enter your Deepgram API key.
+
+
+Launch the application:<br>
+```bash
+node websocket-server
+```
+
+## How this application works
+
+Receives audio from a peer PSTN leg associated to a WebSocket (set up by the Voice API application named pstn-websocket-app.js).<br>
+
+Transcript results from the Deepgram ASR are displayed.
+
+Your actual middleware application may also send audio in the other direction to one or more PSTN legs via the established WebSocket.<br><br>
+
+
+## Other capabilities handled by this sample application code
+
+A WebSocket receives the DTMF events that may be received on the peer PSTN leg. They would be displayed in this application as text payload.<br>
---
+++ websocket-server.js
@@ -0,0 +1,156 @@
+'use strict'
+
+//-------------
+
+require('dotenv').config();
+
+//--
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+const expressWs = require('express-ws')(app);
+app.use(bodyParser.json());
+
+//---- CORS policy - Update this section as needed ----
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "OPTIONS,GET,POST,PUT,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+  next();
+});
+
+// //---- DeepGram ASR engine ----
+
+// const { createClient, LiveTranscriptionEvents } = require("@deepgram/sdk");
+// const fetch = require("cross-fetch");
+// const dgApiKey = process.env.DEEPGRAM_API_KEY;
+
+//-----------------------------------------------------------
+
+app.ws('/socket', (ws, req) => {
+
+  //-- sample custom data --
+  const participant = req.query.participant;
+  const param1 = req.query.attribute1;
+  const param2 = req.query.attribute2;
+
+  console.log('\nNew WebSocket established');
+  console.log('Participant:', participant);
+  console.log('Custom parameter 1:', param1);
+  console.log('Custom parameter 2:', param2);
+
+  //--
+
+  // console.log('Opening client connection to DeepGram');
+
+  // const deepgramClient = createClient(dgApiKey);
+
+  // let deepgram = deepgramClient.listen.live({       
+  //   model: "nova-2",
+  //   smart_format: true,      
+  //   language: "en-US",        
+  //   encoding: "linear16",
+  //   sample_rate: 16000
+  // });
+
+  // deepgram.addListener(LiveTranscriptionEvents.Open, async () => {
+  //   console.log("deepgram: connected");
+
+  //   deepgram.addListener(LiveTranscriptionEvents.Transcript, async (data) => {
+  //     // console.log(JSON.stringify(data));
+  //     const transcript = data.channel.alternatives[0].transcript;
+
+  //     if (transcript != '') {
+
+  //     console.log('\n', participant, 'said:', transcript);
+
+  //     }   
+
+  //   });
+
+  //   deepgram.addListener(LiveTranscriptionEvents.Close, async () => {
+  //     console.log("deepgram: disconnected");
+  //     clearInterval(keepAlive);
+  //     deepgram.finish();
+  //   });
+
+  //   deepgram.addListener(LiveTranscriptionEvents.Error, async (error) => {
+  //     console.log("deepgram: error received");
+  //     console.error(error);
+  //   });
+
+  //   deepgram.addListener(LiveTranscriptionEvents.Warning, async (warning) => {
+  //     console.log("deepgram: warning received");
+  //     console.warn(warning);
+  //   });
+
+  //   deepgram.addListener(LiveTranscriptionEvents.Metadata, (data) => {
+  //     console.log("deepgram: metadata received");
+  //     console.log("ws: metadata sent to client");
+  //     // ws.send(JSON.stringify({ metadata: data }));
+  //     console.log(JSON.stringify({ metadata: data }));
+  //   });
+  
+  // });
+
+  //---------------
+
+  ws.on('message', (msg) => {
+    
+    if (typeof msg === "string") {
+
+      console.log('>>> WebSocket text data:', msg)
+    
+    } else {
+
+      // just for tests
+      // normally the sent audio is from a TTS engine, Voice bot, noise-cancellation engine, sound FX engine, etc ...
+      ws.send(msg);  // echo back received payload
+
+      //---
+      // if (deepgram.getReadyState() === 1 /* OPEN */) {
+      //   deepgram.send(msg);
+      // } else if (deepgram.getReadyState() >= 2 /* 2 = CLOSING, 3 = CLOSED */) {
+      //   // console.log("ws: data couldn't be sent to deepgram");
+      //   null
+      // } else {
+      //   // console.log("ws: data couldn't be sent to deepgram");
+      //   null
+      // }
+      //---
+
+    } 
+
+  });
+
+  //--
+
+  ws.on('close', () => {
+
+    // deepgram.finish();
+    // deepgram.removeAllListeners();
+    // deepgram = null;
+
+    console.log("\n--- WebSocket closed");
+
+  });
+
+});
+
+//-------------------------------------------------------------------------------------------
+
+//--- If this application is hosted on Vonage Cloud Runtime (VCR) serverless infrastructure
+
+app.get('/_/health', async (req, res) => {
+    res.sendStatus(200);
+});
+
+//==================================================
+
+const port = process.env.NERU_APP_PORT || process.env.PORT || 6000;
+
+app.listen(port, () => console.log(`WebSocket server code running on port ${port}.`));
+
+//------------
