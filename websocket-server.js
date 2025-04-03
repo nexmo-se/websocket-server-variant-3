17e0abbb4f65e66a98e27888f81cad2db87b4b0b
Tony Chan <tony.chan@vonage.com>
2025-04-02 17:34:01 -0700

    refactor

--- websocket-server.js
+++ websocket-server.js
@@ -23,9 +23,9 @@ app.use(function (req, res, next) {
 
 // //---- DeepGram ASR engine ----
 
-// const { createClient, LiveTranscriptionEvents } = require("@deepgram/sdk");
-// const fetch = require("cross-fetch");
-// const dgApiKey = process.env.DEEPGRAM_API_KEY;
+const { createClient, LiveTranscriptionEvents } = require("@deepgram/sdk");
+const fetch = require("cross-fetch");
+const dgApiKey = process.env.DEEPGRAM_API_KEY;
 
 //-----------------------------------------------------------
 
@@ -43,55 +43,55 @@ app.ws('/socket', (ws, req) => {
 
   //--
 
-  // console.log('Opening client connection to DeepGram');
+  console.log('Opening client connection to DeepGram');
 
-  // const deepgramClient = createClient(dgApiKey);
+  const deepgramClient = createClient(dgApiKey);
 
-  // let deepgram = deepgramClient.listen.live({       
-  //   model: "nova-2",
-  //   smart_format: true,      
-  //   language: "en-US",        
-  //   encoding: "linear16",
-  //   sample_rate: 16000
-  // });
+  let deepgram = deepgramClient.listen.live({       
+    model: "nova-2",
+    smart_format: true,      
+    language: "en-US",        
+    encoding: "linear16",
+    sample_rate: 16000
+  });
 
-  // deepgram.addListener(LiveTranscriptionEvents.Open, async () => {
-  //   console.log("deepgram: connected");
+  deepgram.addListener(LiveTranscriptionEvents.Open, async () => {
+    console.log("deepgram: connected");
 
-  //   deepgram.addListener(LiveTranscriptionEvents.Transcript, async (data) => {
-  //     // console.log(JSON.stringify(data));
-  //     const transcript = data.channel.alternatives[0].transcript;
+    deepgram.addListener(LiveTranscriptionEvents.Transcript, async (data) => {
+      // console.log(JSON.stringify(data));
+      const transcript = data.channel.alternatives[0].transcript;
 
-  //     if (transcript != '') {
+      if (transcript != '') {
 
-  //     console.log('\n', participant, 'said:', transcript);
+      console.log('\n', participant, 'said:', transcript);
 
-  //     }   
+      }   
 
-  //   });
+    });
 
-  //   deepgram.addListener(LiveTranscriptionEvents.Close, async () => {
-  //     console.log("deepgram: disconnected");
-  //     clearInterval(keepAlive);
-  //     deepgram.finish();
-  //   });
+    deepgram.addListener(LiveTranscriptionEvents.Close, async () => {
+      console.log("deepgram: disconnected");
+      clearInterval(keepAlive);
+      deepgram.finish();
+    });
 
-  //   deepgram.addListener(LiveTranscriptionEvents.Error, async (error) => {
-  //     console.log("deepgram: error received");
-  //     console.error(error);
-  //   });
+    deepgram.addListener(LiveTranscriptionEvents.Error, async (error) => {
+      console.log("deepgram: error received");
+      console.error(error);
+    });
 
-  //   deepgram.addListener(LiveTranscriptionEvents.Warning, async (warning) => {
-  //     console.log("deepgram: warning received");
-  //     console.warn(warning);
-  //   });
+    deepgram.addListener(LiveTranscriptionEvents.Warning, async (warning) => {
+      console.log("deepgram: warning received");
+      console.warn(warning);
+    });
 
-  //   deepgram.addListener(LiveTranscriptionEvents.Metadata, (data) => {
-  //     console.log("deepgram: metadata received");
-  //     console.log(JSON.stringify({ metadata: data }));
-  //   });
+    deepgram.addListener(LiveTranscriptionEvents.Metadata, (data) => {
+      console.log("deepgram: metadata received");
+      console.log(JSON.stringify({ metadata: data }));
+    });
   
-  // });
+  });
 
   //---------------
 
@@ -104,20 +104,20 @@ app.ws('/socket', (ws, req) => {
     } else {
 
       // just for tests
-      // normally the sent audio is from a TTS engine, Voice bot, noise-cancellation engine, sound FX engine, etc ...
-      ws.send(msg);  // echo back received payload
+      // normally if some audio is to be sent to Vonage platform, it is from a TTS engine, Voice bot, noise-cancellation engine, sound FX engine, etc ...
+      // ws.send(msg);  // echo back received payload
 
       //---
-      // if (deepgram.getReadyState() === 1 /* OPEN */) {
-      //   deepgram.send(msg);
-      // } else if (deepgram.getReadyState() >= 2 /* 2 = CLOSING, 3 = CLOSED */) {
-      //   // console.log("ws: data couldn't be sent to deepgram");
-      //   null
-      // } else {
-      //   // console.log("ws: data couldn't be sent to deepgram");
-      //   null
-      // }
-      //---
+
+      if (deepgram.getReadyState() === 1 /* OPEN */) {
+        deepgram.send(msg);
+      } else if (deepgram.getReadyState() >= 2 /* 2 = CLOSING, 3 = CLOSED */) {
+        // console.log("ws: data couldn't be sent to deepgram");
+        null
+      } else {
+        // console.log("ws: data couldn't be sent to deepgram");
+        null
+      }
 
     } 
 
@@ -127,9 +127,9 @@ app.ws('/socket', (ws, req) => {
 
   ws.on('close', () => {
 
-    // deepgram.finish();
-    // deepgram.removeAllListeners();
-    // deepgram = null;
+    deepgram.finish();
+    deepgram.removeAllListeners();
+    deepgram = null;
 
     console.log("\n--- WebSocket closed");
 
@@ -147,7 +147,7 @@ app.get('/_/health', async (req, res) => {
 
 //==================================================
 
-const port = process.env.NERU_APP_PORT || process.env.PORT || 6000;
+const port = process.env.VCR_PORT || process.env.PORT || 6000;
 
 app.listen(port, () => console.log(`WebSocket server code running on port ${port}.`));
 
